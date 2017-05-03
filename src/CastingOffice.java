d6c29ae27de977f9e0b96accdbfb87fdbf0989b7
Kurt <kurtlprice@gmail.com>
2017-05-03 10:19:31 -0700

    Added a comment

--- src/CastingOffice.java
+++ src/CastingOffice.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
  * Created by pricek21 on 5/2/17.
  */
 public class CastingOffice extends Room {
+    //First integer is the rank, second is the amount
     private HashMap<Integer, Integer> rankRequireDollar;
     private HashMap<Integer, Integer> rankRequireCredit;
 
@@ -11,7 +12,12 @@ public class CastingOffice extends Room {
         super(name, availableCount);
     }
 
-    public HashMap<Integer, Integer> getRankRequire(){
+    public HashMap<Integer, Integer> getRankRequireDollar(){
+        return rankRequireDollar;
+    }
+
+    public HashMap<Integer, Integer> getRankRequireCredit(){
+        return rankRequireCredit;
     }
 
     public void rankPlayerCredit(int goalRank, Player player){
