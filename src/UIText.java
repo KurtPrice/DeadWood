5b2eb8237c2e0596f572e4cbb83588bebeb106af
Kurt <kurtlprice@gmail.com>
2017-05-15 09:43:00 -0700

    Added off card role display

--- src/UIText.java
+++ src/UIText.java
@@ -52,9 +52,16 @@ public class UIText {
                                        + ", Active scene: " + currentSceneN
                                        +", Description: "+currentSceneD
                                        +", Budget: " + currentSceneBudget);
-                    if (availRolesScene.length != 0 || availRolesRoom.length != 0){System.out.print("Available Roles: ");}
+                    if (availRolesScene.length != 0 || availRolesRoom.length != 0){
+                        System.out.print("Available Roles: \n");
+                        System.out.println("On-Card Roles: " );
+                    }
+
                     for(Role rs : availRolesScene){
-                        System.out.print("("+rs.getRoleName()+", Rank: "+rs.getRoleRank()+") ");
+                        System.out.print("(" + rs.getRoleName() + ", Rank: " + rs.getRoleRank() + ") ");
+                    }
+                    if(availRolesRoom.length > 0){
+                        System.out.println("\nOff-Card Roles: ");
                     }
                     for(Role rr : availRolesRoom){
                         System.out.print("("+rr.getRoleName()+", Rank: "+rr.getRoleRank()+") ");
